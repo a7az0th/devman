a0d99b24c00140c0673235660b771d146c022810
Alexander Soklev <a7az0th@gmail.com>
2019-08-10 19:43:03 +0300

    Improve timer. Added a secondary class that is WIP still. Used to measure FPS

--- utils/include/timer.h
+++ utils/include/timer.h
@@ -8,27 +8,27 @@ namespace a7az0th {
 typedef long long int int64 ;
 
 // Class providing basic time measurement functionality
-// Stop must be explicitly called!
 class Timer {
 public:
 	// Specify in what precision the timing should be
 	enum class Precision {
 		Seconds = 0,
-		Milliseconds,
-		Nanoseconds,
+		Milliseconds,///< 1/1000 of a second
+		Microseconds,///< 1/1000 of a millisecond
+		Nanoseconds, ///< 1/1000 of a microsecond
 	};
 
 	//Create a timer and start it
 	Timer() { init(); }
 	//Destoy a timer
-	~Timer() {};
+	~Timer() {}
 
 	//Restarts the timer
 	void restart() { init(); }
 
 	// Returns the elapsed time in the precision specified since either the timer was created or last restarted.
 	// whichever occurred sooner
-	int64 elapsed(Precision prec) {
+	int64 elapsed(Precision prec) const {
 		read();
 		int64 val = 0;
 		switch (prec) {
@@ -39,6 +39,11 @@ public:
 		}
 		case Precision::Milliseconds: {
 			const std::chrono::milliseconds& total_ms = std::chrono::duration_cast<std::chrono::milliseconds>(endPoint - startPoint);
+			val = total_ms.count();
+			break;
+		}
+		case Precision::Microseconds: {
+			const std::chrono::microseconds& total_ms = std::chrono::duration_cast<std::chrono::microseconds>(endPoint - startPoint);
 			val = total_ms.count();
 			break;
 		}
@@ -54,16 +59,97 @@ public:
 	}
 
 private:
-	void read() { endPoint = std::chrono::system_clock::now(); }
-	void init() {
-		startPoint = endPoint = std::chrono::system_clock::now();
-	}
+	void read() const { endPoint = std::chrono::system_clock::now(); }
+	void init() const { startPoint = endPoint = std::chrono::system_clock::now(); }
 	// Disallow evil contructors
 	Timer(const Timer&) = delete;
 	Timer& operator=(const Timer&) = delete;
 
-	std::chrono::system_clock::time_point startPoint;
-	std::chrono::system_clock::time_point endPoint;
+	mutable std::chrono::system_clock::time_point startPoint;
+	mutable std::chrono::system_clock::time_point endPoint;
+};
+
+template<int BufferLength>
+struct FPSTimer {
+	FPSTimer() : framesRendered(0), lastFrameTime(0), totalTime(0), summedHistory(0) {
+		history.fill(0);
+	}
+	~FPSTimer() {}
+
+	void start() {
+		t.restart();
+	}
+
+	void stop() {
+		lastFrameTime = t.elapsed(Timer::Precision::Microseconds);
+		int64& historyElement = history[framesRendered % BufferLength];
+		summedHistory = summedHistory - historyElement + lastFrameTime;
+		historyElement = lastFrameTime;
+		totalTime += lastFrameTime;
+		framesRendered++;
+	}
+
+#if 1
+	float getTemporalFPS() const {
+		float res = 0.f;
+		if (summedHistory > 1e-6f) {
+			const int div = (framesRendered < BufferLength) ? framesRendered : BufferLength;
+			res = float((1000000.0*div) / summedHistory);
+		}
+		return res;
+	}
+	float getMomentaryFPS() const {
+		float res = 0.f;
+		if (lastFrameTime > 1e-6f) {
+			res = float(1000000.0 / lastFrameTime);
+		}
+		return res;
+	}
+
+	float getFPS() const {
+		float res = 0.f;
+		if (totalTime > 1e-6f) {
+			res = float((1000000.0*framesRendered) / totalTime);
+		}
+		return res;
+	}
+#else
+	int getTemporalFPS() const {
+		int res = 0;
+		if (summedHistory > 1e-6f) {
+			const int div = (framesRendered < BufferLength) ? framesRendered : BufferLength;
+			res = summedHistory / div;
+		}
+		return res;
+	}
+	int getMomentaryFPS() const {
+		int res = 0;
+		if (lastFrameTime > 1e-6f) {
+			res = lastFrameTime / 1;
+		}
+		return res;
+	}
+
+	int getFPS() const {
+		int res = 0;
+		if (totalTime > 1e-6f) {
+			res = totalTime / framesRendered;
+		}
+		return res;
+	}
+#endif
+
+	int64 sinceLastRestart() const {
+		return t.elapsed(Timer::Precision::Milliseconds);
+	}
+
+private:
+	std::array<int64, BufferLength> history;
+	int64 summedHistory;
+	int framesRendered;
+	int64 totalTime;
+	int64 lastFrameTime;
+	Timer t;
 };
 
 }
